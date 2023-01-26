60b56fc40a7010f72aa48b2b30f2b41768d44ef6
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-26 14:23:06 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,12 @@
+// #### #### Testing #### ####
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++){
+  for (let j = 0; j < yourArray.length; j++){
+    if(myArray[i] === yourArray[j]) {
+      console.log('Both arrays have the number: ' + yourArray[j]);
+    }
+ console.log(myArray[i]+'---'+yourArray[j]) }
+}
+
+// Write your code below
